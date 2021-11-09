3772593687f8d1d3323ce3003ff74386ee3786f5
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2021-11-09 13:52:31 -0300

    Aplicando validacao, refactor e try/except

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 calculation_to_units = 24
 name_of_unit = "hours"
+user_input = ""
 
 def days_to_units(num_of_days) :
-    if num_of_days > 0:
-        return f"{num_of_days} days are {calculation_to_units * num_of_days} {name_of_unit}"
-    elif num_of_days == 0:
-        return "you entered a 0, please enter a valid positive number"
+    return f"{num_of_days} days are {calculation_to_units * num_of_days} {name_of_unit}"
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
+        else:
+            print("you entered a negative number, no conversion for you")
+    except ValueError:
+        print('your input is not a number. Do not ruin my program!')
 
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    calculated_value = days_to_units(user_input_number)
-    print(calculated_value)
-else:
-    print('your input is not a number. Do not ruin my program!')
+while user_input != "exit" :
+    user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+    validate_and_execute()
