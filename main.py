f1628eaf25ffca0f1410c93e8d85e109f5d92466
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2021-11-09 14:13:55 -0300

    Aceitando uma lista de valores

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ def days_to_units(num_of_days) :
 
 def validate_and_execute():
     try:
-        user_input_number = int(user_input)
+        user_input_number = int(num_of_days_element)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number")
+            print(f"you entered {user_input_number}, please enter a valid positive number")
         else:
-            print("you entered a negative number, no conversion for you")
+            print(f"you entered {user_input_number}, no conversion for you")
     except ValueError:
-        print('your input is not a number. Do not ruin my program!')
+        print(f"your input '{num_of_days_element}' is not a number. Do not ruin my program!")
 
 while user_input != "exit" :
-    user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-    validate_and_execute()
+    user_input = input("Hey user, enter number of days as comma separated list and I will convert it to hours!\n")
+    for num_of_days_element in user_input.split(", "):
+        validate_and_execute()
